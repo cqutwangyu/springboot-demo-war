958b3cccf5206cf7200efdf1bd93370c99e7ce36
cqutwangyu <cqwangyu@2016.cqut.edu.cn>
2022-12-14 18:46:50 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootdemowar/SpringbootDemoWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemowar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoWarApplication.class, args);
+    }
+
+}
